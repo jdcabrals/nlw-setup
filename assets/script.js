dd78721eaca3d768f032628aee7dcc8608eda094
jd.cabrals <jd.cabrals@gmail.com>
2023-10-08 10:48:47 -0300

    Added Streak Days count on header 🔥

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
+var i = 0
 
 button.addEventListener("click", add)
 form.addEventListener("change", save)
@@ -17,10 +18,17 @@ function add(){
 
     alert("Dia adicionado com sucesso! ✔")
     nlwSetup.addDay(today)
+    AddDay()
+    
 }
 
 function save(){
     localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+function AddDay(){
+    var h2 = document.getElementById("day")
+    h2.innerHTML = i+1
 }
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
